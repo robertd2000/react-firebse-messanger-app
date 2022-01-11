67ef0c382af66964683a2076060f27d9d0552055
robert d <zdabaghov@mail.ru>
2022-01-11 19:06:14 +0300

    user list

---
+++ src/components/User.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Img from '../image1.jpg'
+
+export const User = ({user, selectUser}) => {
+
+    return (
+        <div className='user_wrapper' onClick={() => selectUser(user)}>
+            <div className='user_info'>
+                <div className='user_detail'>
+                    <img src={user.avatar || Img} alt='avatar' className='avatar' />
+                    <h4>{user.name}</h4>
+                </div>
+                <div className={`user_status ${user.isOnline ? 'online' : 'offline'}`}>
+
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, query, where, onSnapshot } from 'firebase/firestore'
+import { db, auth} from '../firebase'
+import { User } from '../components/User'
 
 export const Home = () => {
+    const [users, setUsers] = useState([])
+    const [chat, setChat] = useState('')
+    useEffect(() => {
+        const userRef = collection(db, 'users')
+        const q = query(userRef, where('uid', 'not-in', [auth.currentUser.uid]))
+        const unsub = onSnapshot(q, snapshot => {
+            const usersArr = []
+            snapshot.forEach(doc => {
+                usersArr.push(doc.data())
+            })
+            setUsers(usersArr)
+        })
+        return () => unsub()
+    }, [])
+
+    const selectUser = (user) => {
+        setChat(user)
+    }
+    
     return (
-        <div>
-            Home
+        <div className='home_container'>
+            <div className='users_container'>
+                {
+                    users.map(user => {
+
+                       return <User key={user.uid} user={user} selectUser={selectUser} />
+                    })
+                }
+            </div>
+            <div className='messages_container'>
+                {
+                    chat ? (<div className='messages_user'>
+                        <h3>{chat.name}</h3>
+                    </div>) : <h3 className='no_conv'>Select user</h3>
+                }
+            </div>
         </div>
     )
 }
